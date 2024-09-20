c537ab0b5dd02299f9e8b438adfbef1ac432a290
gakukek <130475715+gakukek@users.noreply.github.com>
2024-09-20 16:17:04 +0700

    kt files and 16-29 (#3)
    * .kt files done
    
    * 16-29 done

--- app/src/main/java/com/example/lab_week_04/ListFragment.kt
+++ app/src/main/java/com/example/lab_week_04/ListFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.Navigation
 
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
@@ -16,27 +18,40 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ListFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+// Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val coffeeList = listOf<View>(
+            view.findViewById(R.id.affogato),
+            view.findViewById(R.id.americano),
+            view.findViewById(R.id.latte)
+        )
+        coffeeList.forEach{ coffee ->
+            val fragmentBundle = Bundle()
+            fragmentBundle.putInt(COFFEE_ID, coffee.id)
+            coffee.setOnClickListener(
+                Navigation.createNavigateOnClickListener(
+                    R.id.action_listFragment_to_detailFragment,
+                    fragmentBundle)
+            )
+        }
+    }
 
-    companion object {
-        /**
+
+
+                companion object {
+                    private const val COFFEE_ID = "COFFEE_ID"
+
+                    /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
          *
