85f99fcb8aa8f6cc77af39fcdf73d975db82ef56
gakukek <darrylclaylayaya@gmail.com>
2024-09-20 16:01:57 +0700

    .kt files done

--- app/src/main/java/com/example/lab_week_04/DetailFragment.kt
+++ app/src/main/java/com/example/lab_week_04/DetailFragment.kt
@@ -15,25 +15,42 @@ private const val ARG_PARAM2 = "param2"
  * Use the [DetailFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class DetailFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
-
+class DetailFragment<TextView> : Fragment() {
+    private val coffeeTitle: TextView?
+        get() = view?.findViewById(R.id.coffee_title)
+    private val coffeeDesc: TextView?
+        get() = view?.findViewById(R.id.coffee_desc)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+// Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val coffeeId = arguments?.getInt(COFFEE_ID, 0) ?: 0
+        setCoffeeData(coffeeId)
+    }
+    fun setCoffeeData(id: Int){
+        when(id){
+            R.id.affogato -> {
+                coffeeTitle?.text = getString(R.string.affogato_title)
+                coffeeDesc?.text = getString(R.string.affogato_desc)
+            }
+            R.id.americano -> {
+                coffeeTitle?.text = getString(R.string.americano_title)
+                coffeeDesc?.text = getString(R.string.americano_desc)
+            }
+            R.id.latte -> {
+                coffeeTitle?.text = getString(R.string.latte_title)
+                coffeeDesc?.text = getString(R.string.latte_desc)}
+        }
+    }
+
 
     companion object {
         /**
@@ -44,9 +61,10 @@ class DetailFragment : Fragment() {
          * @param param2 Parameter 2.
          * @return A new instance of fragment DetailFragment.
          */
+        private const val COFFEE_ID = "COFFEE_ID"
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            DetailFragment().apply {
+            DetailFragment<Any>().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
